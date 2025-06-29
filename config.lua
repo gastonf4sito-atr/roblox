9d2859e4af066a749678f24382f358b1c381024b
gastonf4sito-atr <gastonqqqqq@gmail.com>
2025-06-29 06:11:57 -0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,5 @@
+local config = {}
+
+config.WEBHOOK_URL = "https://discordapp.com/api/webhooks/1388431336414511165/77u_cpTRj3L-4Q1Tw_zKbiBh7rStQMTKabhnRfpr7k3-zkHZtyqtVQP73dDJBj8if_UO"
+
+return config 
